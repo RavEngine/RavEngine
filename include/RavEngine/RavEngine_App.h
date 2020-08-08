9ca0627a3e63050442f801501b7cbe6de30429af
Ravbug <ravbuganimations@gmail.com>
2020-08-08 15:36:14 -0700

    Refactor some RavEngine_App ~ moved some variables into class definition + current world is now ticked

--- include/RavEngine/RavEngine_App.h
+++ include/RavEngine/RavEngine_App.h
@@ -1,3 +1,13 @@
+#include <OgreRoot.h>
+#include <OgreWindow.h>
+#include <OgreWindowEventUtilities.h>
+
+#ifdef __APPLE__
+#include <OgreMetalRenderSystem.h>
+#elif defined _WIN32
+#include <OgreD3D11RenderSystem.h>
+#endif
+
 
 class RavEngine_App {
 public:
@@ -9,6 +19,29 @@ public:
 	int run(int argc, char** argv);
 	
 protected:
+	class AppWindowEventListener : public Ogre::WindowEventListener
+	{
+		bool mQuit;
+
+	public:
+		AppWindowEventListener() : mQuit(false) {}
+		virtual void windowClosed(Ogre::Window* rw) { mQuit = true; }
+
+		bool getQuit(void) const { return mQuit; }
+	};
+
+	AppWindowEventListener windowEventListener;
+	Ogre::Root* root = nullptr;
+	Ogre::Window* window = nullptr;
+	Ogre::SceneManager* sceneManager;
+
+#ifdef _WIN32
+	typedef Ogre::D3D11RenderSystem NativeRenderSystem;
+#elif defined __APPLE__
+	typedef Ogre::MetalRenderSystem NativeRenderSystem;
+#endif
+	NativeRenderSystem* renderSystem;
+
 	/**
 	The startup hook.
 	@param argc the number of command line arguments
@@ -21,5 +54,6 @@ protected:
 	virtual int OnShutdown() { return 0; };
 
 	void setupwindow();
+
 };
 #define START_APP(APP) int main(int argc, char** argv){APP a; return a.run(argc, argv);}
