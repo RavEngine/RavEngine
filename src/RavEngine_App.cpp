ff14ea371ba50266c3edf990b5a392fed3f8accc
Ravbug <ravbuganimations@gmail.com>
2020-08-11 22:40:12 -0700

    Filament + SDL RenderEngine.cpp + setup Filament singletons + inltialize vulkan backend on Windows with window pointer
    RavEngine_App.hpp
    + restore SDL window event processing

--- src/RavEngine_App.cpp
+++ src/RavEngine_App.cpp
@@ -3,26 +3,70 @@
 
 #include <GameplayStatics.hpp>
 #include "RenderEngine.hpp"
+#include <SDL_events.h>
 
 using namespace std;
 
 int RavEngine_App::run(int argc, char** argv) {
 
 	//invoke startup hook
-    setupwindow();
 	OnStartup(argc, argv);
-    
-    RenderEngine::Init();
 
-    while (true) {
-
-    }
+	auto inputManager = GameplayStatics::inputManager;
+	int width = 0; int height = 0;
     
+	bool exit = false;
+	SDL_Event event;
+	while (!exit) {
+		while (SDL_PollEvent(&event)) {
+
+			switch (event.type) {
+			case SDL_QUIT:
+				exit = true;
+				break;
+
+			case SDL_WINDOWEVENT: {
+				const SDL_WindowEvent& wev = event.window;
+				switch (wev.event) {
+				case SDL_WINDOWEVENT_RESIZED:
+				case SDL_WINDOWEVENT_SIZE_CHANGED:
+					SDL_GetWindowSize(RenderEngine::GetWindow(), &width, &height);
+					//reset_screen(width, height);
+					break;
+
+				case SDL_WINDOWEVENT_CLOSE:
+					exit = true;
+					break;
+				}
+			} break;
+
+			case SDL_KEYDOWN:
+			case SDL_KEYUP:
+				inputManager->SDL_key(event.key.state, event.key.keysym.scancode);
+				break;
+			case SDL_MOUSEMOTION:
+				inputManager->SDL_mousemove((float)event.motion.x / width, (float)event.motion.y / height, event.motion.xrel, event.motion.yrel);
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+			case SDL_MOUSEBUTTONUP:
+				inputManager->SDL_mousekey(event.button.state, event.button.button);
+				break;
+			case SDL_CONTROLLERAXISMOTION:
+			case SDL_CONTROLLER_AXIS_LEFTX:
+			case SDL_CONTROLLER_AXIS_LEFTY:
+				inputManager->SDL_ControllerAxis(event.caxis.axis + Special::CONTROLLER_AXIS_OFFSET, (event.caxis.value) / ((float)SHRT_MAX));
+				break;
+			case SDL_CONTROLLERBUTTONDOWN:
+			case SDL_CONTROLLERBUTTONUP:
+				inputManager->SDL_mousekey(event.cbutton.state + Special::CONTROLLER_BUTTON_OFFSET, event.cbutton.button);
+				break;
+			case SDL_CONTROLLERDEVICEADDED:
+			case SDL_CONTROLLERDEVICEREMOVED:
+				break;
+			}
+			GameplayStatics::currentWorld->tick();
+		}
+	}
 
     return OnShutdown();
-}
-
-
-void RavEngine_App::setupwindow(){
-
 }
