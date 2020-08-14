769a198105b971c2b9e21bbd781d0bef60bb9923
Ravbug <ravbuganimations@gmail.com>
2020-08-14 11:22:14 -0700

    Hello Triangle via linking to FilamentApp

--- src/RenderEngine.cpp
+++ src/RenderEngine.cpp
@@ -29,6 +29,8 @@
 #include <utils/EntityManager.h>
 #include <filament/TransformManager.h>
 #include <filament/Skybox.h>
+#include <filamentapp/FilamentApp.h>
+#include <filamentapp/Config.h>
 
 #include <SDL_syswm.h>
 #include <SDL.h>
@@ -60,6 +62,94 @@ static const Vertex TRIANGLE_VERTICES[3] = {
 };
 
 static constexpr uint16_t TRIANGLE_INDICES[3] = { 0, 1, 2 };
+
+void test() {
+	struct App {
+		VertexBuffer* vb;
+		IndexBuffer* ib;
+		Material* mat;
+		filament::Camera* cam;
+		utils::Entity camera;
+		Skybox* skybox;
+		utils::Entity renderable;
+	};
+	Config config;
+	config.title = "hellotriangle";
+
+	//material
+	string mat;
+	{
+		auto path = "deps\\filament\\filament\\generated\\material\\defaultMaterial.filamat";
+		ifstream fin(path, ios::binary);
+		ostringstream buffer;
+		buffer << fin.rdbuf();
+		mat = buffer.str();
+	}
+
+	App app;
+	auto setup = [&app, &mat](Engine* engine, View* view, Scene* scene) {
+		app.skybox = Skybox::Builder().color({ 0.1, 0.125, 0.25, 1.0 }).build(*engine);
+		scene->setSkybox(app.skybox);
+		view->setPostProcessingEnabled(false);
+		static_assert(sizeof(Vertex) == 12, "Strange vertex size.");
+		app.vb = VertexBuffer::Builder()
+			.vertexCount(3)
+			.bufferCount(1)
+			.attribute(VertexAttribute::POSITION, 0, VertexBuffer::AttributeType::FLOAT2, 0, 12)
+			.attribute(VertexAttribute::COLOR, 0, VertexBuffer::AttributeType::UBYTE4, 8, 12)
+			.normalized(VertexAttribute::COLOR)
+			.build(*engine);
+		app.vb->setBufferAt(*engine, 0,
+			VertexBuffer::BufferDescriptor(TRIANGLE_VERTICES, 36, nullptr));
+		app.ib = IndexBuffer::Builder()
+			.indexCount(3)
+			.bufferType(IndexBuffer::IndexType::USHORT)
+			.build(*engine);
+		app.ib->setBuffer(*engine,
+			IndexBuffer::BufferDescriptor(TRIANGLE_INDICES, 6, nullptr));
+		app.mat = Material::Builder()
+			.package(mat.c_str(), mat.size())
+			.build(*engine);
+		app.renderable = utils::EntityManager::get().create();
+		RenderableManager::Builder(1)
+			.boundingBox({ { -1, -1, -1 }, { 1, 1, 1 } })
+			.material(0, app.mat->getDefaultInstance())
+			.geometry(0, RenderableManager::PrimitiveType::TRIANGLES, app.vb, app.ib, 0, 3)
+			.culling(false)
+			.receiveShadows(false)
+			.castShadows(false)
+			.build(*engine, app.renderable);
+		scene->addEntity(app.renderable);
+		app.camera = utils::EntityManager::get().create();
+		app.cam = engine->createCamera(app.camera);
+		view->setCamera(app.cam);
+	};
+
+	auto cleanup = [&app](Engine* engine, View*, Scene*) {
+		engine->destroy(app.skybox);
+		engine->destroy(app.renderable);
+		engine->destroy(app.mat);
+		engine->destroy(app.vb);
+		engine->destroy(app.ib);
+		engine->destroyCameraComponent(app.camera);
+		utils::EntityManager::get().destroy(app.camera);
+	};
+
+	FilamentApp::get().animate([&app](Engine* engine, View* view, double now) {
+		constexpr float ZOOM = 1.5f;
+		const uint32_t w = view->getViewport().width;
+		const uint32_t h = view->getViewport().height;
+		const float aspect = (float)w / h;
+		app.cam->setProjection(filament::Camera::Projection::ORTHO,
+			-aspect * ZOOM, aspect * ZOOM,
+			-ZOOM, ZOOM, 0, 1);
+		auto& tcm = engine->getTransformManager();
+		tcm.setTransform(tcm.getInstance(app.renderable),
+			filament::math::mat4f::rotation(now, filament::math::float3{ 0, 0, 1 }));
+		});
+
+	FilamentApp::get().run(config, setup, cleanup);
+}
 
 /**
 Construct a render engine instance
@@ -204,6 +294,9 @@ void RenderEngine::Init()
 	if (filamentEngine != nullptr) {
 		return;
 	}
+
+	test();
+
 	//create SDL window
 
 	SDL_Init(SDL_INIT_EVENTS);
