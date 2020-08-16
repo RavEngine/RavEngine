fbeef2d3ab84a9f2374e9fbc2528cdc8f76a5632
Ravbug <ravbuganimations@gmail.com>
2020-08-15 17:27:58 -0700

    Move triangle mesh out of RenderEngine StaticMesh.hpp + members for storing vb and ib
    RenderEngine.cpp
    + add child entities on spawn
    
    StaticMesh.cpp
    + hard-code the triangle

--- include/RavEngine/StaticMesh.hpp
+++ include/RavEngine/StaticMesh.hpp
@@ -1,12 +1,19 @@
-
 #include "Component.hpp"
 #include <vector>
 #include "Material.hpp"
+#include <utils/Entity.h>
+#include <math/mat2.h>
 
 namespace filament {
     class VertexBuffer;
     class IndexBuffer;
 }
+
+
+struct Vertex {
+    filament::math::float2 position;
+    uint32_t color;
+};
 
 namespace RavEngine {
     class StaticMesh : public Component {
@@ -28,11 +35,21 @@ namespace RavEngine {
             return material;
         }
 
+        void AddHook(const WeakRef<RavEngine::Entity>&) override;
+
     protected:
-        //std::vector<PosColorVertex> vertices;
-        //std::vector<int> triList;
 
         //the default material
         Ref<Material> material;
+
+        //index and vertex buffers, stores actual data
+        std::vector<Vertex> vb;
+        std::vector<unsigned int> ib;
+
+        //stores pointers into vectors for rendering (does not duplicate data)
+        filament::VertexBuffer* fvb = nullptr;
+        filament::IndexBuffer* fib = nullptr;
+
+        utils::Entity renderable;
     };
 }
