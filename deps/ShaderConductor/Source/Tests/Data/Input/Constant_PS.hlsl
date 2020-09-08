1755d8b6198c95e5a93f74d3dfd93c6901da3bcc
Ravbug <ravbuganimations@gmail.com>
2020-09-08 16:47:20 -0700

    Early version of ShaderTranspiler

--- deps/ShaderConductor/Source/Tests/Data/Input/Constant_PS.hlsl
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-float4 PSMain() : SV_Target
-{
-    return float4(0.2f, 0.4f, 0.6f, 1.0f);
-}
--- deps/ShaderConductor/Source/Tests/Data/Input/IncludeEmptyHeader.hlsl
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#include "Inc/HeaderEmpty.hlsli"
-
-float4 main() : SV_Target
-{
-    return 0;
-}
