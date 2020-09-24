b7a114179a5dca13e8767175c558d85d944e5437
Ravbug <ravbuganimations@gmail.com>
2020-09-23 21:45:37 -0700

    Fix undefined behavior in Transform Transform.cpp ~ use Set instead of Delta to explicitly set values

--- src/Transform.cpp
+++ src/Transform.cpp
@@ -19,9 +19,9 @@ RavEngine::Transform::~Transform()
 Transform::Transform(const vector3& inpos, const quaternion& inrot, const vector3& inscale, bool inStatic) {
 
 	matrix = matrix4(1);
-	LocalTranslateDelta(inpos);
-	LocalRotateDelta(inrot);
-	LocalScaleDelta(inscale);
+	SetLocalPosition(inpos);
+	SetLocalRotation(inrot);
+	SetLocalScale(inscale);
 	isStatic = inStatic;
 
 	Apply();
