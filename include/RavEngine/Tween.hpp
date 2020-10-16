d5e8dce337b6217de976441b93ebee1dc6f29060
Ravbug <ravbuganimations@gmail.com>
2020-10-16 10:49:33 -0700

    Tweeny wrappers Tween.cpp + namespaced templated wrapper constexpr structs for Tweeny's built-ins
    GameWorld.cpp
    ~ update namespace

--- include/RavEngine/Tween.hpp
+++ include/RavEngine/Tween.hpp
@@ -6,6 +6,7 @@
 
 namespace RavEngine{
 
+namespace TweenCurves{
 	//template wrappers around Tweeny
 	template<typename base>
 	struct TweenCurve : public base{
@@ -15,25 +16,41 @@ namespace RavEngine{
 			return base::template run<float>(pos, a,b);
 		}
 	};
+	static constexpr struct DefaultCurve : public TweenCurve<tweeny::easing::defaultEasing>{} DefaultCurve;
+	static constexpr struct SteppedCurve : public TweenCurve<tweeny::easing::steppedEasing>{} SteppedCurve;
+	static constexpr struct LinearCurve : public TweenCurve<tweeny::easing::linearEasing>{} LinearCurve;
+	static constexpr struct QuadraticInCurve : public TweenCurve<tweeny::easing::quadraticInEasing>{} QuadraticInCurve;
+	static constexpr struct QuadraticOutCurve : public TweenCurve<tweeny::easing::quadraticOutEasing>{} QuadraticOutCurve;
+	static constexpr struct QuadraticInOutCurve : public TweenCurve<tweeny::easing::quadraticInOutEasing>{} QuadraticInOutCurve;
+	static constexpr struct CubicInCurve : public TweenCurve<tweeny::easing::cubicInEasing>{} CubicInCurve;
+	static constexpr struct CubicOutCurve : public TweenCurve<tweeny::easing::cubicOutEasing>{} CubicOutCurve;
+	static constexpr struct CubicInOutCurve : public TweenCurve<tweeny::easing::cubicInOutEasing>{} CubicInOutCurve;
+	static constexpr struct QuarticInCurve : public TweenCurve<tweeny::easing::quarticInEasing>{} QuarticInEasing;
+	static constexpr struct QuarticOutCurve : public TweenCurve<tweeny::easing::quarticOutEasing>{} QuarticOutCurve;
+	static constexpr struct QuarticInOutCurve : public TweenCurve<tweeny::easing::quarticInOutEasing>{} QuarticInOutCurve;
+	static constexpr struct QuinticInCurve : public TweenCurve<tweeny::easing::quinticInEasing>{} QuinticInCurve;
+	static constexpr struct QuinticOutCurve : public TweenCurve<tweeny::easing::quinticOutEasing>{} QuinticOutCurve;
+	static constexpr struct QuinticInOutCurve : public TweenCurve<tweeny::easing::quinticInOutEasing>{} QuinticInOutCurve;
+	static constexpr struct SinusoidalInCurve : public TweenCurve<tweeny::easing::sinusoidalInEasing>{} SinusoidalInCurve;
+	static constexpr struct SinusoidalOutCurve : public TweenCurve<tweeny::easing::sinusoidalOutEasing>{} SinusoidalOutCurve;
+	static constexpr struct SinusoidalInOutCurve : public TweenCurve<tweeny::easing::sinusoidalInOutEasing>{} SinusoidalInOutCurve;
+	static constexpr struct ExponentialInCurve : public TweenCurve<tweeny::easing::exponentialInEasing>{} ExponentialInCurve;
+	static constexpr struct ExponentialOutCurve : public TweenCurve<tweeny::easing::exponentialOutEasing>{} ExponentialOutCurve;
+	static constexpr struct ExponentialInOutCurve : public TweenCurve<tweeny::easing::exponentialInOutEasing>{} ExponentialInOutCurve;
+	static constexpr struct CircularInCurve : public TweenCurve<tweeny::easing::circularInEasing>{} CircularInCurve;
+	static constexpr struct CircularOutCurve : public TweenCurve<tweeny::easing::circularOutEasing>{} CircularOutCurve;
+	static constexpr struct CircularInOutCurve : public TweenCurve<tweeny::easing::circularInOutEasing>{} CircularInOutCurve;
+	static constexpr struct BounceInCurve : public TweenCurve<tweeny::easing::bounceInEasing>{} BounceInCurve;
+	static constexpr struct BounceOutCurve : public TweenCurve<tweeny::easing::bounceOutEasing>{} BounceOutCurve;
+	static constexpr struct BounceInOutCurve : public TweenCurve<tweeny::easing::bounceInOutEasing>{} BounceInOutCurve;
+	static constexpr struct ElasticInCurve : public TweenCurve<tweeny::easing::elasticInEasing>{} ElasticInCurve;
+	static constexpr struct ElasticOutCurve : public TweenCurve<tweeny::easing::elasticOutEasing>{} ElasticOutCurve;
+	static constexpr struct ElasticInOutCurve : public TweenCurve<tweeny::easing::elasticOutEasing>{} ElasticInOutCurve;
+	static constexpr struct BackInCurve : public TweenCurve<tweeny::easing::backInEasing>{} BackInCurve;
+	static constexpr struct BackOutCurve : public TweenCurve<tweeny::easing::backOutEasing>{} BackOutCurve;
+	static constexpr struct BackInOutCurve : public TweenCurve<tweeny::easing::backOutEasing>{} BackInOutCurve;
+}
 
-	static constexpr struct DefaultCurveCL : public TweenCurve<tweeny::easing::defaultEasing>{} DefaultCurve;
-	static constexpr struct SteppedCurveCL : public TweenCurve<tweeny::easing::steppedEasing>{} SteppedCurve;
-	static constexpr struct LinearCurveCL : public TweenCurve<tweeny::easing::linearEasing>{} LinearCurve;
-	static constexpr struct QuadraticInCurveCL : public TweenCurve<tweeny::easing::quadraticInEasing>{} QuadraticInCurve;
-	static constexpr struct QuadraticOutCurveCL : public TweenCurve<tweeny::easing::quadraticOutEasing>{} QuadraticOutCurve;
-	static constexpr struct QuadraticInOutCurveCL : public TweenCurve<tweeny::easing::quadraticInOutEasing>{} QuadraticInOutCurve;
-	static constexpr struct CubicInCurveCL : public TweenCurve<tweeny::easing::cubicInEasing>{} CubicInCurve;
-	static constexpr struct CubicOutCurveCL : public TweenCurve<tweeny::easing::cubicOutEasing>{} CubicOutCurve;
-	static constexpr struct CubicInOutCurveCL : public TweenCurve<tweeny::easing::cubicInOutEasing>{} CubicInOutCurve;
-	static constexpr struct QuarticInCurveCL : public TweenCurve<tweeny::easing::quarticInEasing>{} QuarticInEasing;
-	static constexpr struct QuarticOutCurveCL : public TweenCurve<tweeny::easing::quarticOutEasing>{} QuarticOutCurve;
-	static constexpr struct QuarticInOutCurveCL : public TweenCurve<tweeny::easing::quarticInOutEasing>{} QuarticInOutCurve;
-	static constexpr struct QuinticInCurveCL : public TweenCurve<tweeny::easing::quinticInEasing>{} QuinticInCurve;
-	static constexpr struct QuinticOutCurveCL : public TweenCurve<tweeny::easing::quinticOutEasing>{} QuinticOutCurve;
-	static constexpr struct QuinticInOutCurveCL : public TweenCurve<tweeny::easing::quinticInOutEasing>{} QuinticInOutCurve;
-	static constexpr struct SinusoidalInCurveCL : public TweenCurve<tweeny::easing::sinusoidalInEasing>{} SinusoidalInCurve;
-	static constexpr struct SinusoidalOutCurveCL : public TweenCurve<tweeny::easing::sinusoidalOutEasing>{} SinusoidalOutCurve;
-	static constexpr struct SinusoidalInOutCurveCL : public TweenCurve<tweeny::easing::sinusoidalInOutEasing>{} SinusoidalInOutCurve;
 
 
 	template<typename ...Floats>
@@ -41,6 +58,8 @@ namespace RavEngine{
 	protected:
 		tweeny::tween<Floats...> anim;
 		typedef std::function<void(Floats...)> stepfunc;
+		
+		
 		
 	public:
 		Tween(){};
