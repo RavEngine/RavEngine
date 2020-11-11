3e6d9fdd2634a35e8083d949cff2ed78d21bb759
Ravbug <ravbuganimations@gmail.com>
2020-11-11 14:25:49 -0800

    Add script to delete nonessential parts of dependencies

---
+++ deps/shrink-deps.sh
@@ -0,0 +1,13 @@
+# PhysX
+rm -r physx/physx/documentation/
+rm -r physx/physx/samples/
+
+# BGFX, BX
+rm -r bgfx.cmake/bgfx/examples
+rm -r bgfx.cmake/bx/tools
+
+# SDL
+rm -r SDL2/test
+
+# assimp
+rm -r assimp/samples
