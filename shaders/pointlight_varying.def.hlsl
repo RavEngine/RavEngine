554cbf6199ce8808a8dacb8cbf688caf5f61caea
Ravbug <ravbuganimations@gmail.com>
2020-12-17 10:54:16 -0800

    Fix DirectX backend, TEXCOORD8 does not exist

--- shaders/pointlight_varying.def.hlsl
+++ shaders/pointlight_varying.def.hlsl
@@ -1,10 +1,10 @@
 vec3 a_position  : POSITION;
 
-vec4 i_data0 : TEXCOORD8;
-vec4 i_data1 : TEXCOORD7;
-vec4 i_data2 : TEXCOORD6;
-vec4 i_data3 : TEXCOORD5;
-vec4 i_data4 : TEXCOORD4;
+vec4 i_data0 : TEXCOORD7;
+vec4 i_data1 : TEXCOORD6;
+vec4 i_data2 : TEXCOORD5;
+vec4 i_data3 : TEXCOORD4;
+vec4 i_data4 : TEXCOORD3;
 
 vec4 colorintensity : TEXCOORD2;
 vec4 positionradius : TEXCOORD1;
