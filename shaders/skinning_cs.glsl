06c82c4d827050331d1d7d79066f0decab33431a
Ravbug <ravbuganimations@gmail.com>
2021-04-22 21:28:54 -0700

    Adjust shader

--- shaders/skinning_cs.glsl
+++ shaders/skinning_cs.glsl
@@ -1,12 +1,12 @@
 #include "common.sh"
 #include <bgfx_compute.sh>
 
-BUFFER_WR(skinmatrix, vec4, 0);
-BUFFER_RO(bindpose, vec4, 1);
-BUFFER_RO(weights, vec2, 2);
-BUFFER_RO(pose, vec4, 3);
+BUFFER_WR(skinmatrix, vec4, 0);	// 4x4 matrices
+BUFFER_RO(bindpose, vec4, 1);	// 4x4 matrices
+BUFFER_RO(weights, vec2, 2);	// index, influence
+BUFFER_RO(pose, vec4, 3);	 	// 4x4 matrices
 
-uniform vec4 NumObjects;
+uniform vec4 NumObjects;		// x = num objects, y = num vertices
 
 const int NUM_INFLUENCES = 4;
 
@@ -18,8 +18,7 @@ void main()
 		return;
 	}
 	
-	const int weightsid = gl_GlobalInvocationID.y * NumObjects.y * 4 + gl_GlobalInvocationID.x * 4;
-	const int vertoffset = gl_GlobalInvocationID.y * NumObjects.y * 2 + gl_GlobalInvocationID.x;
+	const int weightsid = gl_GlobalInvocationID.y * NumObjects.y * 2 + gl_GlobalInvocationID.x * 2;
 	
 	mat4 totalmtx = mtxFromRows(
 								vec4(1,0,0,0),
@@ -29,24 +28,25 @@ void main()
 								);
 	
 	for(int i = 0; i < NUM_INFLUENCES; i++){
-		float weight = weights[weightsid + i + 1];
-		int joint_idx = weights[weightsid + i * 4] ;
+		vec2 weightdata = weights[weightsid + i];
+		float weight = weightdata.y;
+		int joint_idx = weightdata.x ;
 		
 		mat4 posed_mtx;
-		posed_mtx[0] = pose[joint_idx];
-		posed_mtx[1] = pose[joint_idx+1];
-		posed_mtx[2] = pose[joint_idx+2];
-		posed_mtx[3] = pose[joint_idx+3];
+		posed_mtx[0] = pose[joint_idx * 4];
+		posed_mtx[1] = pose[joint_idx * 4 + 1];
+		posed_mtx[2] = pose[joint_idx * 4 + 2];
+		posed_mtx[3] = pose[joint_idx * 4 + 3];
 		
 		mat4 bindpose_mtx;
-		bindpose_mtx[0] = bindpose[vertoffset];
-		bindpose_mtx[1] = bindpose[vertoffset+1];
-		bindpose_mtx[2] = bindpose[vertoffset+2];
-		bindpose_mtx[3] = bindpose[vertoffset+3];
+		bindpose_mtx[0] = bindpose[joint_idx * 4];
+		bindpose_mtx[1] = bindpose[joint_idx * 4 + 1];
+		bindpose_mtx[2] = bindpose[joint_idx * 4 + 2];
+		bindpose_mtx[3] = bindpose[joint_idx * 4 + 3];
 		
 		posed_mtx = posed_mtx * bindpose_mtx;
 		
-		//totalmtx += (posed_mtx * weight);
+		totalmtx += (weight * posed_mtx);
 	}
 	//destination to write the matrix
 	const int offset = gl_GlobalInvocationID.y * NumObjects.y * 4 + gl_GlobalInvocationID.x * 4;
