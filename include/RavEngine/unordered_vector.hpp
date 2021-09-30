c9cf5c777a5371be19d2781013b5a64cb3a075fe
Ravbug <ravbuganimations@gmail.com>
2021-09-30 15:29:28 -0400

    Make unordered_vector customizable

--- include/RavEngine/unordered_vector.hpp
+++ include/RavEngine/unordered_vector.hpp
@@ -1,28 +1,104 @@
 #pragma once
 #include <vector>
 #include <phmap.h>
+#include <functional>
 
 namespace RavEngine {
 
-template<typename T>
-struct unordered_vector : public std::vector<T>{
+/**
+ The Unordered Vector provides:
+ - O(1) erase by iterator
+ All other complexities for valid behaviors are identical to a regular vector. Elements must be moveable.
+ Note that the order of elements cannot be guareneed.
+ */
+template<typename T, typename vec = std::vector<T>>
+class unordered_vector{
+    vec underlying;
+public:
+    typedef typename decltype(underlying)::iterator iterator_type;
+    typedef typename decltype(underlying)::const_iterator const_iterator_type;
+    typedef typename decltype(underlying)::size_type index_type;
+    typedef typename decltype(underlying)::size_type size_type;
 
-    constexpr inline void erase(const typename std::vector<T>::iterator& it){
-		*it = std::move(this->back());
-		this->pop_back();
+    /**
+     Erase by iterator. Complexity is O(1).
+     @param it the iterator to erase
+     */
+    inline void erase(iterator_type it){
+        *it = std::move(underlying.back());
+        underlying.pop_back();
 	}
 	
+    /**
+     Erase by iterator. Complexity is O(n)
+     @param value the data to erase
+     */
 	inline void erase(const T& value){
-		auto it = std::find(this->begin(),this->end(),value);
-		erase(it);
+        auto it = std::find(underlying.begin(),underlying.end(),value);
+        underlying.erase(it);
 	}
 	
+    /**
+     Add an item to the container
+     @param value the data to add
+     */
 	inline void insert(const T& value){
-		this->push_back(value);
+        underlying.push_back(value);
 	}
+    
+    inline T& operator[](index_type idx){
+        return underlying[idx];
+    }
+    
+    const inline T& operator[](index_type idx) const{
+        return underlying[idx];
+    }
+    
+    inline T& at(index_type idx){
+        return underlying.at(idx);
+    }
+    
+    const inline T& at(index_type idx) const{
+        return underlying.at(idx);
+    }
+    
+    inline const_iterator_type begin() const{
+        return underlying.begin();
+    }
+    
+    inline const_iterator_type end() const{
+        return underlying.end();
+    }
+    
+    inline iterator_type end(){
+        return underlying.end();
+    }
+    
+    inline iterator_type begin(){
+        return underlying.begin();
+    }
+    
+    inline size_type size() const{
+        return underlying.size();
+    }
+    
+    inline void reserve(){
+        underlying.reserve();
+    }
+    
+    inline void resize(){
+        underlying.resize();
+    }
 	
 };
 
+/**
+ The Unordered Cached Vector provides:
+ - O(1) erase by value
+ - O(1) erase by iterator
+ All other complexities are identical to a regular vector. Elements must be moveable and hashable.
+ Note that the order of elements cannot be guareneed.
+ */
 template<typename T>
 class unordered_cached_vector : public std::vector<T>{
 protected:
