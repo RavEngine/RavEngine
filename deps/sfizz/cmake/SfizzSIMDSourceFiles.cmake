e84f5c96ea41230e2106948ae7e59c01cfa87191
Ravbug <ravbuganimations@gmail.com>
2022-11-23 10:36:05 -0800

    No -msimd128 on gcc

--- deps/sfizz/cmake/SfizzSIMDSourceFiles.cmake
+++ deps/sfizz/cmake/SfizzSIMDSourceFiles.cmake
@@ -12,12 +12,17 @@ macro(sfizz_add_simd_sources SOURCES_VAR PREFIX)
     if(SFIZZ_SYSTEM_PROCESSOR MATCHES "^(x86_64|amd64|AMD64|x64|X64|i.86|x86|X86)$")
         # on GCC, it requires to set ISA support flags on individual files
         # to be able to use the intrinsics
+		
+        if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+			set(SIMD128 "-msimd128")
+		endif()
+        
         if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
             set_source_files_properties(
                 ${PREFIX}/sfizz/effects/impl/ResonantStringAVX.cpp
                 ${PREFIX}/sfizz/effects/impl/ResonantArrayAVX.cpp
                 ${PREFIX}/sfizz/simd/HelpersAVX.cpp
-                PROPERTIES COMPILE_FLAGS "-mavx -msimd128")
+                PROPERTIES COMPILE_FLAGS "-mavx ${SIMD128}")
         endif()
     endif()
 endmacro()
