efced82871bc9242105662a468896bd350789d25
Ravbug <ravbuganimations@gmail.com>
2022-12-16 19:31:46 -0800

    Add more graph API

--- include/RavEngine/AudioGraphAsset.hpp
+++ include/RavEngine/AudioGraphAsset.hpp
@@ -4,6 +4,10 @@
 
 namespace RavEngine{
 
+/**
+* Represents an audio effect graph processor. For a list of 
+* nodes, see https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API
+*/
 class AudioGraphAsset{
     
     lab::OfflineContext audioContext;
@@ -11,7 +15,12 @@ class AudioGraphAsset{
     uint8_t nchannels = 0;
     
 public:
-    AudioGraphAsset(const lab::AudioStreamConfig& config, uint8_t nchannels);
+    /**
+    * Create an AudioGraphAsset.
+    * @param nchannels the number of channels. It is dependent on where you want to use this Asset. 
+    * If you are using it on point sources, then nchannels should be 1, because pre-spatialized audio is mono. If you are using it after all spatialization is complete, like on the AudioListener, or you are using it on an AmbientAudioSource in which case no processing occurred, then nchannels should be set to the number of output channels for your application. If you are using this asset for something else, then set nchannels accordingly. 
+    */
+    AudioGraphAsset(uint8_t nchannels);
     
     /**
      Render the graph given input samples
@@ -25,6 +34,22 @@ public:
     decltype(audioContext.context)& GetContext(){
         return audioContext.context;
     }
+
+    auto GetInputNode() const {
+        return inputBus;
+    }
+
+    auto GetOutputNode() const{
+        return outputBus;
+    }
+
+    /**
+    * Connect two audio nodes within this asset
+    * @param source the node providing data
+    * @param destination the node to provide data to
+    * @note this argument order is reversed comapred to WebAudio
+    */
+    auto Connect(std::shared_ptr<lab::AudioNode> source, std::shared_ptr<lab::AudioNode> dest);
 };
 
 }
--- src/AudioGraphAsset.cpp
+++ src/AudioGraphAsset.cpp
@@ -1,9 +1,13 @@
 #include "AudioGraphAsset.hpp"
+#include "AudioPlayer.hpp"
 
 using namespace RavEngine;
 
-AudioGraphAsset::AudioGraphAsset(const lab::AudioStreamConfig& config, uint8_t nchannels) :
-    audioContext(lab::MakeOfflineAudioContext(config)),
+AudioGraphAsset::AudioGraphAsset(uint8_t nchannels) :
+    audioContext(lab::MakeOfflineAudioContext(lab::AudioStreamConfig{
+            .desired_channels = nchannels,
+            .desired_samplerate = static_cast<float>(AudioPlayer::GetSamplesPerSec()),
+        })),
     outputBus(std::make_shared<lab::AudioBus>(nchannels,0,false)),  // we will give it the input data and size when it's time to render
     inputBus(std::make_shared<lab::AudioBus>(nchannels,0, false)),
     nchannels(nchannels)
@@ -24,6 +28,12 @@ void AudioGraphAsset::Render(PlanarSampleBufferInlineView& inout, PlanarSampleBu
     std::swap(inout, scratchBuffer);
     
 }
+
+auto RavEngine::AudioGraphAsset::Connect(std::shared_ptr<lab::AudioNode> source, std::shared_ptr<lab::AudioNode> dest)
+{
+    audioContext.context->connect(dest, source);
+}
+
 
 
 void AudioGraphComposed::renderImpl(PlanarSampleBufferInlineView inputSamples, PlanarSampleBufferInlineView intermediateBuffer, uint8_t nchannels){
