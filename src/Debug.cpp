dd300ef8944b8f2288dd346557aa258eb2676022
Ravbug <ravbug@users.noreply.github.com>
2024-02-11 21:45:52 -0500

    No SDL_log in server mode

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -2,7 +2,9 @@
 #include "Debug.hpp"
 #include <chrono>
 #include <date/date.h>
+#if !RVE_SERVER
 #include <SDL_log.h>
+#endif
 
 using namespace RavEngine;
 SpinLock Debug::mtx;
@@ -10,7 +12,11 @@ SpinLock Debug::mtx;
 void RavEngine::Debug::LogHelper(FILE* output, const std::string_view message, const char* type) {
 	auto date = date::format("%F %T", std::chrono::system_clock::now());
 	mtx.lock();
+#if !RVE_SERVER
 	SDL_Log("[%s] %s - %s\n", date.c_str(), type, message.data());
+#else
+    fprintf(output, "[%s] %s - %s\n", date.c_str(), type, message.data());
+#endif
 	mtx.unlock();
 }
 
