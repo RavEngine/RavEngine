c5845127c8eb4697c6cd7c8e48e297f8a24e3a24
Ravbug <ravbuganimations@gmail.com>
2024-03-25 14:16:43 -0400

    Update Arch.hpp

--- include/RavEngine/Arch.hpp
+++ include/RavEngine/Arch.hpp
@@ -2,7 +2,7 @@
 #include <cstdint>
 
 // 32-bit detection. if __WORDSIZE does not exist, then we'll just assume 64 bit.
-#if __WORDSIZE == 32
+#if __WORDSIZE == 32 || __EMSCRIPTEN__
     #define RVE_32_BIT 1
 #else
     #define RVE_64_BIT 1
