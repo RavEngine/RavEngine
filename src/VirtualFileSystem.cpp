f4ea297e45b4c89a3b954cc4709b90f190558a30
Ravbug <ravbug@noreply.users.github.com>
2024-05-18 20:54:06 -0700

    An incredibly cursed solution to the Android file reading problem courtesy of this blog! https://blog.apicici.com/posts/2023/04/using-sdl2-to-mount-an-archive-from-the-android-apk-assets-with-physicsfs/

--- src/VirtualFileSystem.cpp
+++ src/VirtualFileSystem.cpp
@@ -7,6 +7,125 @@
     #include <CoreFoundation/CFBundle.h>
 #endif
 
+#if __ANDROID__
+#include <SDL_iostream.h>
+
+// Android sucks and makes reading data out of the Apk difficult.
+// we must emulate some behavior in order for PhysFS to work with it.
+
+// this code is adapted from https://blog.apicici.com/posts/2023/04/using-sdl2-to-mount-an-archive-from-the-android-apk-assets-with-physicsfs/
+
+struct sdl_rw_file_info {
+    std::string name;
+    SDL_IOStream* RW = nullptr;
+    Sint64 cursor_pos = 0;
+    Sint64 size = 0;
+};
+
+static PHYSFS_sint64 io_read(PHYSFS_Io* io, void* buffer, PHYSFS_uint64 len) {
+    auto finfo = (sdl_rw_file_info*)io->opaque;
+    const PHYSFS_uint64 bytes_left = (finfo->size - finfo->cursor_pos);
+    PHYSFS_sint64 bytes_read;
+
+    if (bytes_left < len) { len = bytes_left; }
+
+    bytes_read = SDL_ReadIO(finfo->RW, buffer, len);
+
+    if (bytes_read > 0) {
+        finfo->cursor_pos += bytes_read;
+    }
+    else {
+        SDL_ClearError();
+        bytes_read = SDL_GetError()[0] != '\0' ? -1 : 0;
+    }
+
+    return bytes_read;
+}
+
+static int io_seek(PHYSFS_Io* io, PHYSFS_uint64 offset) {
+    auto finfo = (sdl_rw_file_info*)io->opaque;
+
+    if (offset >= finfo->size) {
+        PHYSFS_setErrorCode(PHYSFS_ERR_PAST_EOF);
+        return 0;
+    }
+
+    finfo->cursor_pos = SDL_SeekIO(finfo->RW, offset, SDL_IO_SEEK_SET);
+    return finfo->cursor_pos >= 0;
+}
+
+static PHYSFS_sint64 io_tell(PHYSFS_Io* io) {
+    auto finfo = (sdl_rw_file_info*)io->opaque;
+    return finfo->cursor_pos;
+}
+
+static PHYSFS_sint64 io_length(PHYSFS_Io* io) {
+    auto finfo = (sdl_rw_file_info*)io->opaque;
+    return finfo->size;
+}
+
+
+static void io_destroy(PHYSFS_Io* io) {
+    auto finfo = (sdl_rw_file_info*)io->opaque;
+    SDL_CloseIO(finfo->RW);
+    delete finfo;
+    delete io;
+}
+
+static PHYSFS_Io* io_duplicate(PHYSFS_Io* io);
+
+PHYSFS_Io* GetAndroidIo(const char* path){
+    SDL_IOStream* RW = SDL_IOFromFile(path, "rb");
+    if (!RW){
+        return nullptr;
+    }
+
+    auto finfo = new sdl_rw_file_info;
+    finfo->RW = RW;
+    finfo->size = SDL_GetIOSize(RW);
+    if (finfo->size < 0) {
+        SDL_CloseIO(RW);
+        delete finfo;
+        return nullptr;
+    }
+    finfo->cursor_pos = 0;
+    finfo->name = path;
+
+    auto retval = new PHYSFS_Io;
+
+    retval->version = 0;
+    retval->opaque = (void*)finfo;
+    retval->read = io_read;
+    retval->write = nullptr; // read-only
+    retval->seek = io_seek;
+    retval->tell = io_tell;
+    retval->length = io_length;
+    retval->duplicate = io_duplicate;
+    retval->flush = nullptr; // read-only
+    retval->destroy = io_destroy;
+
+    return retval;
+}
+
+static PHYSFS_Io* io_duplicate(PHYSFS_Io* io) {
+    auto origfinfo = (sdl_rw_file_info*)io->opaque;
+    auto retval = GetAndroidIo(origfinfo->name.c_str());
+
+    if (!retval) { return nullptr; }
+
+    auto finfo = (sdl_rw_file_info*)retval->opaque;
+    finfo->cursor_pos = origfinfo->cursor_pos;
+    // seek the SDL_RWops to the appropriate position
+    if (SDL_SeekIO(finfo->RW, finfo->cursor_pos, SDL_IO_SEEK_SET) < 0) {
+        retval->destroy(retval); // free allocated memory
+        return nullptr;
+    }
+
+    return retval;
+}
+
+#endif
+
 // defined in client user libraries automatically by cmake
 // if the user does not define these, the library will not link
 extern const std::span<const char> cmrc_get_file_data(const std::string_view& path);
@@ -45,10 +164,31 @@ VirtualFilesystem::VirtualFilesystem() {
 
     streamingAssetsPath = streamingAssetsPath / Format("{}_Streaming",path);
 
-	//1 means add to end, can put 0 to make it first searched
+#if __ANDROID__
+    // we need to do some additional setup here. Android `assets` are not accessible to C functions
+    // out of the box. we must copy them to a readable location using the Android api
+
+    PHYSFS_Io* androidIo = GetAndroidIo(cstr);
+    if(!androidIo){
+        Debug::Fatal("Android error: Cannot load {}", cstr);
+    }
+
+    if (PHYSFS_mountIo(androidIo, cstr, "", 1) == 0){
+        Debug::Fatal("PHYSFS Android Error: {}", PHYSFS_WHY());
+
+        // PHYSFS_mountIo does not destroy the PHYSFS_Io if it fails
+        // so we do it manually
+        androidIo->destroy(androidIo);
+    }
+
+#else
+    //1 means add to end, can put 0 to make it first searched
 	if (PHYSFS_mount(cstr, "", 1) == 0) {
 		Debug::Fatal("PHYSFS Error: {}",PHYSFS_WHY());
 	}
+#endif
+
+
 	auto root = PHYSFS_enumerateFiles("/");
 	if (*root == NULL) {
 		Debug::Fatal("PHYSFS Error: {}", PHYSFS_WHY());
