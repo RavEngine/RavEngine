83cb6f9818fd9c7052e457348fdde1af828e878f
Ravbug <ravbug@noreply.users.github.com>
2024-06-09 20:53:38 -0700

    Templates strike again

--- include/RavEngine/Format.hpp
+++ include/RavEngine/Format.hpp
@@ -18,7 +18,7 @@ constexpr auto Format(fmt_src::format_string<Args...> formatstr, Args&& ... args
 
 template<typename ... T>
 auto VFormat(const std::string_view formatstr, T&& ... args){
-    return fmt_src::vformat(formatstr, fmt_src::make_format_args(std::forward<T>(args)...));
+    return fmt_src::vformat(formatstr, fmt_src::make_format_args(args...));
 }
 
 }
