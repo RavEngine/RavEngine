3cae61965a90714cfdf09d6fd6a0f8de0cd43d39
Ravbug <ravbug@noreply.users.github.com>
2025-01-31 21:29:25 -0800

    Better way of testing compatibility, thanks MJP!

--- include/RavEngine/MaterialShared.hpp
+++ include/RavEngine/MaterialShared.hpp
@@ -7,64 +7,31 @@ namespace RavEngine {
 	};
 
 	struct MeshAttributes {
-		struct {
-			bool position : 1 = false;
-			bool normal : 1 = false;
-			bool tangent : 1 = false;
-			bool bitangent : 1 = false;
-			bool uv0 : 1 = false;
-			bool lightmapUV : 1 = false;
+		union {
+			struct {
+				bool position : 1;
+				bool normal : 1;
+				bool tangent : 1;
+				bool bitangent : 1;
+				bool uv0 : 1;
+				bool lightmapUV : 1;
+				bool padding : 2;
+			};
+			uint8_t bits = 0;
 		};
 
 		bool operator==(const MeshAttributes& other) const {
-			return
-				position == other.position &&
-				normal == other.normal &&
-				tangent == other.tangent &&
-				bitangent == other.bitangent &&
-				uv0 == other.uv0 &&
-				lightmapUV == other.lightmapUV
-				;
+			return bits == other.bits;
 		}
+
 
 		/**
 		* `this` is compatible with `other` if for every bit set to true in `this`, the corresponding bit in `other` is also set to true.
 		* It does not matter if `other` has additional bits set.
 		*/
-		bool CompatibleWith(const MeshAttributes& other) const{
-			// someone PLEASE refactor this to be less stupid.
-			if (position) {
-				if (not other.position) {
-					return false;
-				}
-			}
-			if (normal) {
-				if (not other.normal) {
-					return false;
-				}
-			}
-			if (tangent) {
-				if (not other.tangent) {
-					return false;
-				}
-			}
-			if (bitangent) {
-				if (not other.bitangent) {
-					return false;
-				}
-			}
-			if (uv0) {
-				if (not other.uv0) {
-					return false;
-				}
-			}
-			if (lightmapUV) {
-				if (not other.lightmapUV) {
-					return false;
-				}
-			}
-			return true;
+		bool CompatibleWith(const MeshAttributes& other) const {
+			return (bits & other.bits) == bits;
 		}
 	};
-
+		
 }
