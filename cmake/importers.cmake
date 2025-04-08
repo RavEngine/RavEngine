26df83c140d131225a6f36d5c246665c6aff08cd
Ravbug <ravbug@users.noreply.github.com>
2025-04-07 18:41:05 -0700

    update zlib because assimps is too old

--- cmake/importers.cmake
+++ cmake/importers.cmake
@@ -2,7 +2,7 @@
 file(GLOB SRC "${CMAKE_CURRENT_LIST_DIR}/../tools/importlib/*.cpp" "${CMAKE_CURRENT_LIST_DIR}/../tools/importlib/*.hpp" "${CMAKE_CURRENT_LIST_DIR}/../tools/importlib/*.h")
 add_library(rve_importlib ${SRC})
 target_compile_features(rve_importlib PRIVATE cxx_std_20)
-target_link_libraries(rve_importlib PRIVATE assimp fmt glm)
+target_link_libraries(rve_importlib PRIVATE assimp fmt glm zlibstatic)
 target_include_directories(rve_importlib 
 	PRIVATE "${CMAKE_CURRENT_LIST_DIR}/../include/RavEngine" "${CMAKE_CURRENT_LIST_DIR}/../tools/importlib/RavEngine" "${CMAKE_CURRENT_LIST_DIR}/../deps/parallel-hashmap/parallel_hashmap"
 	PUBLIC "${CMAKE_CURRENT_LIST_DIR}/../tools/importlib"
